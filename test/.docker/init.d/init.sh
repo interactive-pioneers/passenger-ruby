d5e3669a6cf7a65420d2212fd8a587f44cbc49d2
Ain Tohvri <at@interactive-pioneers.de>
2016-05-03 14:05:46 +0200

    Resolve #1 Tests

---
+++ test/.docker/init.d/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ $RAILS_ENV == 'development' ]; then
+  echo "Adjusting uid/gid for local development..."
+  usermod -u 1000 app
+  usermod -G staff app
+fi
---
+++ test/test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+docker-compose exec web bundle
+
+components="mysql psql npm node webpack bower"
+
+for component in $components
+do
+  docker-compose exec web which $component
+done
+
+exit 0
