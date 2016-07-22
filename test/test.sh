caf66bafca8946482e0727b2ea3b328322263748
Ain Tohvri <ain@flashbit.net>
2016-07-22 16:34:58 +0200

    Add support for Ruby 2.1

--- test/test.sh
+++ test/test.sh
@@ -2,18 +2,26 @@
 
 set -e
 
-docker-compose exec web bundle
-
+echo "Testing version 2.3"
+docker-compose exec web23 bundle
 components="mysql psql npm node webpack bower convert"
-
-echo "Testing components $components..."
-
 for component in $components
 do
-  if [[ -z  $(docker-compose exec web which $component) ]]; then
+  if [[ -z  $(docker-compose exec web23 which $component) ]]; then
     echo "$component not found! Test failed."
     exit 1
   fi
 done
+echo "2.3 test complete. All components identified."
 
-echo "Test complete. All components there."
+echo "Testing version 2.1"
+docker-compose exec web21 bundle
+components="psql npm node webpack bower convert"
+for component in $components
+do
+  if [[ -z  $(docker-compose exec web21 which $component) ]]; then
+    echo "$component not found! Test failed."
+    exit 1
+  fi
+done
+echo "2.1 test complete. All components identified."
