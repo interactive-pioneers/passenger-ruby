6d8640d53c8f1dd00a6b6d61f33c84c34c066586
Ain Tohvri <at@interactive-pioneers.de>
2016-07-26 15:08:41 +0200

    Fix file path tests

--- test/test.sh
+++ test/test.sh
@@ -14,28 +14,31 @@ function test {
 
   docker-compose exec $container bundle
 
+  printf "\nChecking components..."
   for component in $components
   do
     if [[ -z  $(docker-compose exec $container bash -lc "which $component") ]]; then
       printf "\n$component not found! Test failed.\n"
       exit 1
+    else
+      printf "\n$component found."
     fi
   done
 
+  printf "\nChecking filepaths..."
   for filepath in $filepaths
   do
-    if [[ -z  $(docker-compose exec $container test -e $filepath) ]]; then
-      printf "\n$filepath not found! Test failed."
-      exit 1
-    fi
+    docker-compose exec $container test -e $filepath \
+      && printf "\n$filepath found." \
+      || (printf "\n$filepath not found! Test failed.\n"; exit 1)
   done
 
-  printf "\n$version test complete. All components identified.\n"
+  printf "\n$version test complete. All tests successful.\n"
 }
 
 dependencies="mysql psql npm node webpack bower convert ruby"
 files="/etc/nginx/sites-enabled/test.conf /home/app/Gemfile"
 
-test 2.1.5-rvm web215rvm "$dependencies rvm" "$files ~/.bash_profile"
+test 2.1.5-rvm web215rvm "$dependencies rvm" "$files /root/.bash_profile"
 test 2.1 web21 $dependencies $files
 test 2.3 web23 $dependencies $files
