9a17570dec828dc75b91aba06c1db6d3696b6517
Ain Tohvri <at@interactive-pioneers.de>
2016-07-27 17:14:24 +0200

    Add HTTP response test

--- test/test.sh
+++ test/test.sh
@@ -15,18 +15,23 @@ function test {
   printf "\nChecking dependencies...\n"
   for dependency in "${dependencies[@]}"
   do
-    docker-compose exec $container bash -lc "which $dependency" || (printf "\n$dependency not found! Test failed.\n"; exit 1)
+    docker-compose exec $container bash -lc "which $dependency" || (printf "\n$dependency not found! Test failed."; exit 1)
   done
 
-  printf "\nChecking filepaths..."
+  printf "\n\nChecking filepaths...\n"
   for filepath in "${filepaths[@]}"
   do
     docker-compose exec $container test -e $filepath \
       && printf "\n$filepath found." \
-      || (printf "\n$filepath not found! Test failed.\n"; exit 1)
+      || (printf "\n$filepath not found! Test failed."; exit 1)
   done
 
-  printf "\n$version test complete. All tests successful.\n"
+  printf "\n\nChecking HTTP response..."
+  host=$(docker-compose port $container 80)
+  http=$(curl -I http://$host 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+  [ "$http" == "200" ] && printf "\nResponse OK" || (printf "\nResponse failure ($http)!"; exit 1)
+
+  printf "\n\n$version test complete. All tests successful.\n"
 }
 
 declare -a dependencies=(mysql psql npm node webpack bower convert ruby wget rvm)
