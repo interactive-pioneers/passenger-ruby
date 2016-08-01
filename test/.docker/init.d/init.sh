707eb9a354ae1235168de5fa392d17801033043c
Ain Tohvri <at@interactive-pioneers.de>
2016-08-01 10:52:04 +0200

    Fix port check failure in tests

--- test/.docker/init.d/init.sh
+++ test/.docker/init.d/init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [ $RAILS_ENV == 'development' ]; then
+if [[ $RAILS_ENV == 'development' ]]; then
   echo "Adjusting uid/gid for local development..."
   usermod -u 1000 app
   usermod -G staff app
 fi
+
+sudo service apache2 start
